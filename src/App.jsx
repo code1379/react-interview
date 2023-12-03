856e79e3a8280cdb1a3cff1fdca3803e0450311b
code1379 <coderxialuo@gmail.com>
2023-12-03 22:18:54 +0800

    feat: 事件委托（事件代理）及18合成事件挂载载根节点root上

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,77 @@
-import React from "./react";
-
-/**
- * 定义一个类组件集成自父类React.Component
- * 定义的类组件必须编写（实现）一个名为 render 的函数，负责返回要渲染的虚拟DOM
- */
+import React from "react";
 class ClassComponent extends React.Component {
-  constructor(props) {
-    super(props); // this.props = props
-    // 在内部会把收集到的属性对象放在自己的实例上，以后可以通过this.props访问
-  }
+  parentBubble = () => {
+    console.log("父节点 冒泡阶段执行");
+  };
 
+  childBubble = () => {
+    console.log("子节点 冒泡阶段执行");
+  };
+
+  parentCapture = () => {
+    console.log("父节点 捕获阶段执行");
+  };
+
+  childCapture = () => {
+    console.log("子节点 捕获阶段执行");
+  };
   render() {
-    const { name } = this.props;
-    return <div>hello {name}</div>;
+    return (
+      <div
+        id="parent"
+        onClick={this.parentBubble}
+        onClickCapture={this.parentCapture}
+      >
+        <button
+          id="child"
+          onClick={this.childBubble}
+          onClickCapture={this.childCapture}
+        >
+          click
+        </button>
+      </div>
+    );
   }
 }
 
-// babel 会把属性收集起来变成一个props属性对象，并传递给类组件的构造函数
-// const App = <ClassComponent name="zhangsan"/>
+setTimeout(() => {
+  // 不能直接使用 parent.addEventListener 因为 window 上有 默认的 parent 属性
+  const parent = document.getElementById("parent");
+  const child = document.getElementById("child");
+
+  parent.addEventListener("click", () => {
+    console.log("原生的 父节点 冒泡阶段执行");
+  });
+
+  child.addEventListener("click", () => {
+    console.log("原生的 子节点 冒泡阶段执行");
+  });
+
+  parent.addEventListener(
+    "click",
+    () => {
+      console.log("原生的 父节点 捕获阶段执行");
+    },
+    true
+  );
+
+  child.addEventListener(
+    "click",
+    () => {
+      console.log("原生的 子节点 捕获阶段执行");
+    },
+    true
+  );
+}, 500);
+/**
+  父节点 捕获阶段执行
+  子节点 捕获阶段执行
+  原生的 父节点 捕获阶段执行
+  原生的 子节点 捕获阶段执行
+  原生的 子节点 冒泡阶段执行
+  原生的 父节点 冒泡阶段执行 
+  子节点 冒泡阶段执行 
+  父节点 冒泡阶段执行
+ */
 const App = React.createElement(ClassComponent, { name: "lisi" });
 export default App;
